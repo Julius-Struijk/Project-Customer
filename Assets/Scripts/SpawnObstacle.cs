0861a30c20052877d5663470dc23fdd9aaa17e39
Julius Struijk <julius.struijk@gmail.com>
2024-09-18 18:19:47 +0200

    Made Object Spawning more general

--- Assets/Scripts/SpawnObstacle.cs
+++ Assets/Scripts/SpawnObstacle.cs
@@ -7,6 +7,8 @@ public class SpawnObstacle : MonoBehaviour
 
     [SerializeField] int spawnChancePercentage = 50;
     [SerializeField] List<GameObject> obstacles;
+    
+    float verticalBuffer = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +23,18 @@ public class SpawnObstacle : MonoBehaviour
         {
             GameObject obstacleToSpawn = obstacles[Random.Range(0, obstacles.Count)];
 
+            // Changing which scale is added based on the obstacle's rotation.
             float spawnX = Random.Range(roadPosition.x - road.transform.localScale.x / 2 + obstacleToSpawn.transform.localScale.x, roadPosition.x + road.transform.localScale.x / 2 - obstacleToSpawn.transform.localScale.x);
-            float spawnY = roadPosition.y + obstacleToSpawn.transform.localScale.y + 1;
+
+            float spawnY;
+            if (obstacleToSpawn.transform.rotation.z >= 0.5f) { spawnY = roadPosition.y + obstacleToSpawn.transform.localScale.x + verticalBuffer; }
+            else {  spawnY = roadPosition.y + obstacleToSpawn.transform.localScale.y + verticalBuffer; }
+
             float spawnZ = Random.Range(roadPosition.z - road.transform.localScale.z / 2, roadPosition.z + road.transform.localScale.z / 2);
+
             Vector3 spawnPosition = new Vector3(spawnX , spawnY, spawnZ);
             Instantiate(obstacleToSpawn, spawnPosition, obstacleToSpawn.transform.rotation);
-            Debug.Log("Spawned new coin at: " + spawnPosition);
+            Debug.Log("Spawned new obstacle at: " + spawnPosition);
         }
     }
 
